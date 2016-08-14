462028e61fa53c0e8c55fa8e38a5ea71aaa75500
Jesse Kuras <jesse.kuras2@gmail.com>
2016-08-14 14:10:18 -0700

    (67)-Red-Dot-Activity_official_add-the-others-were-flukes

---
+++ PSTourGuide/app/src/main/java/com/pugetsound/pstourguide/pstourguide/RedDotActivity.java
@@ -0,0 +1,13 @@
+package com.pugetsound.pstourguide.pstourguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RedDotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_red_dot);
+    }
+}
